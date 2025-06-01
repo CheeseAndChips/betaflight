0340030df269f03bb5ce3eff06110d49696f2bb8
Joris Pevcevičius <joris.pevcas@gmail.com>
2025-06-01 13:40:13 +0300

    command TX formatting

--- src/main/sensors/windspeed.h
+++ src/main/sensors/windspeed.h
@@ -1,4 +1,34 @@
 #pragma once
 
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
 #include <stdbool.h>
 
+#define WINDSPEED_TX_BUFFER_SIZE (64)
+
+typedef enum {
+    windspeedParseOk = 0,
+    windspeedParseNoDollar,
+    windspeedParseNoComma,
+    windspeedParseNoAsterisk,
+    windspeedParseNoCRLF,
+    windspeedParseBadIDLength,
+    windspeedParseBadChecksumLength,
+} windspeedParseError_t;
+
+const uint16_t WINDSPEED_INVALID_CHECKSUM = 0xffff;
+
+typedef char windspeedEncodedID_t[2];
+typedef char windspeedEncodedChecksum_t[2];
+
+typedef struct {
+    char *payload_start;
+    windspeedEncodedID_t id;
+    windspeedEncodedChecksum_t checksum;
+} windspeedParsedResponse_t;
+
+typedef struct {
+    char buffer[WINDSPEED_TX_BUFFER_SIZE];
+    size_t buffer_filled;
+} windspeedTxBuffer_t;
